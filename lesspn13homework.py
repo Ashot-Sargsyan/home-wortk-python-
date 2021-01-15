adf6b1d235767c9c73d77d71be9e31f3eb0b4991
Ashot-Sargsyan <77446410+Ashot-Sargsyan@users.noreply.github.com>
2021-01-15 23:56:54 +0400

    Tuple Data Types

---
+++ lesspn13homework.py
@@ -0,0 +1,55 @@
+'''1)Write a Python program to count the elements in a list until an element is a  tuple.'''
+# num=[10,20,30,(10,20,),40]#dasi vaxt noric veranael lav chem haskacel
+# c=0
+# for n in num:
+# 	if isinstance(n,tuple):
+# 		break
+# 	c+=1
+# print(c)
+
+
+'''2)Write a Python program to reverse a tuple'''
+
+# number=(1,2,5,6,8,9,10,0)
+# y=reversed(number)
+# print(tuple(y))
+
+'''2.1)Write a Python program to reverse a tuple'''
+
+# word=('car','book','king','bananas')
+# y=reversed(word)
+# print(tuple(y))
+
+
+'''3)Write a Python program to find the length of a tuple.'''
+# word=('car','book','king','bananas')
+# y=reversed(word)
+# print(tuple(y),'\n',len(word))
+
+'''3.1)Write a Python program to find the length of a tuple.'''
+# number=(1,2,5,6,8,9,10,0)
+# y=reversed(number)
+# print(tuple(y),'\n',len(number))
+
+
+'''4)Write a Python program to convert a tuple to a string'''
+# word=('c','a','r',' ','b','o','o','k',' ','k','i','n','g')
+# mystr=''.join(word)
+# print(mystr)
+
+
+'''5)Write a Python program which calculate the count of item in tuple'''
+
+# tuple1=1
+# tuple2=15
+# tuple3=12
+# tuple4=tuple1+tuple2+tuple3
+# print(tuple4)
+
+
+'''6)Write a Python program to find name in tuple'''
+# name=('apple','samsung','toshiba','huawei','google')
+# if 'apple' in name:
+# 	print("Yes there is one on our list 'apple' ")
+# else:
+# 	print('no there is one on our list in apple')
