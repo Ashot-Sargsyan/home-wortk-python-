8e4fee80be4bc87ea66c176b1eda9a25b3bdd1ca
Ashot-Sargsyan <77446410+Ashot-Sargsyan@users.noreply.github.com>
2021-01-15 23:49:43 +0400

    Logical Operators

---
+++ lesspn7homework.py
@@ -0,0 +1,76 @@
+# '''1)We write a program where we buy to a phone'''
+# phone=input('Do you have iphone and samsung') == 'yes' 
+# model=input('Do you have iphone 11 pro max or iphone 12 pro') == 'yes' 
+# iphone=input('You are buying to iphone and green color')=='yes'
+# result=phone and model and iphone
+# if result:
+# 	print('Buy to ')
+# else:
+# 	print('There is no phone in our store that suits you')
+
+
+
+# '''2)We writen a program where a chek 5  question '''
+# question1=input('Where you were born')=='Russia'
+# question2=input('Where are you living now')== 'Armenia'
+# question3=int(input('How old do you live in Armenia'))==15
+# question4=input('Do you study')=='yes'
+# question5=input('Do you a work')== 'yes'
+# result=question1 and question2 and question3 and question4 or question5
+# if result:
+# 	print('Very well you are hired')
+# else:
+# 	print('Sorry but you do not fit us')
+
+
+
+'''3)We writen a program where cheak age employee'''
+# import random
+# age=random.randint(17,25)
+# result_age=age>=17 or age<=25
+# print(age,result_age)
+# higher_education=input('do you have higher_education? ')=='yes'
+# work=input('do you experience?')=='yes'
+# human=result_age and work or higher_education
+# if human:
+# 	print('Very well','\n','your age is :',age,'\n','you fit in our age in company:',result_age,'\n','your higher_education',higher_education,'\n','your experience is  not bad',work)
+# else:
+# 	print('you didnt enter work our office')
+
+
+
+'''4)We write a program buy to dream car'''
+# cars ="BMW Mercedes-Benz Hundai Honda Audi Subaru Tesla"
+# company=input('We have many different types of machines')
+# if company in cars:
+# 	print('Let is go,Buy to cars',company.upper())
+# else:
+# 	print('Unfortunately, we do not have such a car')
+
+
+
+'''5)We write a program math (a+b)**3'''
+# a=5
+# b=6
+# x=(a+b)**3
+# print(x)
+
+# x=a**3+3*a**2*b+3*a*b**2+b**3
+# print(x)
+
+
+'''6)We write a program '''
+# import random
+# computer=random.randint(1,10)
+# human='1,2,6,8,10'#stringa 
+# res=str(computer) in human#tive bara darcnum str
+# if res :
+# 	print('my number is :',human ,'\n','number computer is :','\n',computer,'\n',res,)
+# else:
+# 	print('your number is FALSE EROR 5005',computer)
+	
+
+# '''7)Factorial'''
+# import math
+# number=int(input('My naumber is :'))
+# print(math.factorial(number))
