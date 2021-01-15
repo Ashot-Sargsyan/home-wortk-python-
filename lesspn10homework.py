d13da1456b0229b9ecc7ce04fd2414d61167ef03
Ashot-Sargsyan <77446410+Ashot-Sargsyan@users.noreply.github.com>
2021-01-15 23:53:27 +0400

    Loop

---
+++ lesspn10homework.py
@@ -0,0 +1,37 @@
+'''1)We writen programa python when we check number siri equal human'''
+# import random
+# siri=random.randint(1,20)
+# count=1
+# while True:
+# 	human=int(input('human number:'))
+# 	if 21<human or human<=0  :
+# 		print('please enter a number from (1,20)')
+# 	elif human==siri:
+# 		print('human number and number siri is equal',siri)
+# 		break
+# 		count+=1
+# 	else:
+# 		print('Error')
+	
+
+
+
+
+	
+'''2)Chinga Chung'''
+# import random
+# point=0
+# while True:
+# 	game='rps '
+# 	siri=random.choice(game)
+# 	human=input('rps')
+# 	# if siri=='scissors' and human=='scissors':
+# 	# 	print('siri and human is equal')
+# 	if siri=='r' and human=='p' or siri=='p' and  human=='s' or siri=='s' and human=='r':
+# 		point+=1
+# 		print('Human is win ',point)
+# 		break
+# 	elif siri=='r' and human=='s' or siri=='s' and human=='p' and siri=='p' and human=='r':
+# 		point+=1
+# 		print('Siri is win',point)
+# 		break
