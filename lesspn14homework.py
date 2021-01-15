fcb42d6518a6fea77b8b9e583e8b1491f93b2662
Ashot-Sargsyan <77446410+Ashot-Sargsyan@users.noreply.github.com>
2021-01-16 00:02:57 +0400

    List Data Types

---
+++ lesspn14homework.py
@@ -0,0 +1,110 @@
+'''1)Write a Python program to sum all the items in a list.'''
+# number=[12,15,26,40,50,0,8,66]
+# word=['book','apple','orange','phone','pc']
+# number.extend(word)
+# print(number)
+
+
+'''2)Write a Python program to multiplies all the items in a list.'''
+# number=[12,15,26,40,50,8,66]
+# x=1
+# for i in number:
+# 	x*=i
+# 	print(x)
+
+'''3)Write a Python program to get the largest number from a list.'''
+# number=[12,15,-26,40,50,0,-8.50,66]
+# print(max(number))
+
+
+'''4)Write a Python program that have two lists and returns True if they have at least one common member.'''
+# number1=[12,15,26,40,50,8,66]
+# number2=[10,16,22,42,50,0]
+# for i in number1:
+# 	for x in number2:
+# 		if i==x:
+# 			print('yes one common member is :',x,True)
+# 			break
+# 		else:
+# 			print('sorry but you dont have any member')
+			
+
+'''5)Write a Python program add an element at the end '''
+# word=['book','apple','orange','phone','pc']
+# word.append('king')
+# print(word)
+
+
+'''5)Write a Python program add an element at the end for numbers '''
+# number=[12,15,26,40,50,8,66]
+# number.append(44)
+# print(number)
+
+
+'''6)Write a Python program cleaning the element'''
+# word=['book','apple','orange','phone','pc']#aysiqn chenq kara nshenq clear(2) kam clear(orange) vor jnji?
+# word.clear()
+# print(word)
+
+
+'''7)Write a Python program cleaning the element for number'''
+# number=[12,15,26,40,50,8,66]
+# number.clear()
+# print(number)
+
+
+'''8)Write a Python program copy list'''
+# list1=['book','apple','orange','phone','pc']
+# list2=list1.copy()
+# print(list2,'\n',list1)
+
+'''9)Write a Python program count'''
+# number=[12,15,26,40,50,8,66]#cuica tali kani hat 26 ka 
+# x=number.count(26)
+# print(x)
+
+'''10)Write a Python program count for all elements'''
+# my_list=[12,15,60,'book','king']
+# x=my_list.count('book')cuica tali kani hat book ka
+# print(x)
+
+'''11)Write a Python program extend'''
+# fruits=['banana','apple','cherry','kiwi']
+# number=[12,15,26,40,50,8,66]
+# fruits.extend(number)#fruitsi mech gruma numbere
+# print(fruits)
+
+'''12)Write a Python program index'''
+# fruits=['banana','apple','cherry','kiwi']
+# x=fruits.index('apple')#applee vor texna eta cuic tali
+# print(x)
+
+
+'''13)Write a Python program insert'''
+# fruits=['banana','apple','cherry','kiwi']
+# fruits.insert(0,'mango')#mangon grumaarachuc vorehetev asuma 0 texum gri aysiqn bananic arach 
+# print(fruits)
+
+
+'''14)Write a Python program pop'''
+# fruits=['banana','apple','cherry','kiwi']
+# fruits.pop(1)#ste pope hanuma 1 index unecox elemente aysiqn apple 
+# print(fruits)
+
+
+'''15)Write a Python program remove'''
+# fruits=['banana','apple','cherry','kiwi']
+# fruits.remove('cherry')
+# print(fruits)
+
+
+'''16)Write a Python program reverse'''
+# fruits = ['apple', 'banana', 'cherry']
+# fruits.reverse()#hetevic aracha grum 
+# print(fruits)
+
+
+'''17)Write a Python program sort'''
+# number=[12,15,26,40,50,8,66]
+# number.sort()#mecic poqr sortavoruma 
+# print(number)
