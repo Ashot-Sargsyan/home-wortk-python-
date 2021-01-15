9d636c319ee98a3fce219dc864191bf9282c8a57
Ashot-Sargsyan <77446410+Ashot-Sargsyan@users.noreply.github.com>
2021-01-15 23:46:58 +0400

    import Modules

---
+++ lesspn5homework.py
@@ -0,0 +1,74 @@
+'''1)We  have (1,10)  random number'''
+# import random
+# print(random.randint(1,10))
+
+
+'''2)We check human number (1,10) random siri'''
+# import random as r
+# human=float(input('human number is:'))
+# siri=r.randint(10,100)
+# result1=human>=siri
+# result2=human!=siri
+# result3=human<=siri
+# print('human number:',human,'\n','siri number is:',siri,'\n',result1,result2,result3)
+
+
+'''3)My birhday '''
+# import datetime
+# birhday=datetime.datetime(1996,4,21,20,30)
+# print('My birhday:',birhday)
+
+'''4)My birhday in calendar'''
+# import calendar
+# year=int(input('My year is :'))
+# month=int(input('My month is:'))
+# day=int(input('My day is :'))
+# print(calendar.month(year,month,day))
+
+
+'''5)We write to Discriminate(4x**2-14x+10=0)'''
+# import math
+# import time
+# a=4
+# b=14
+# c=10
+# Discriminate=b**2-4*a*c
+# print(Discriminate)
+# x1=b+math.sqrt(Discriminate)/(2*a)
+# x2=b-math.sqrt(Discriminate)/(2*a)
+# time.sleep(5)
+# print('Our Discriminate :',Discriminate,x1,x2)
+
+
+'''6)We have a radius circle'''
+# import math
+# import random
+# random=random.randint(0,10)
+# r=2*math.pi*2*random**2
+# print(random,r)
+
+
+'''7)We game Siri (throw the dice)'''
+# import random
+# human2=random.randint(1,6)
+# human1=random.randint(1,6)
+# siri1=random.randint(1,6)
+# siri2=random.randint(1,6)
+# human=human1+human2
+# siri=siri1+siri2
+# res1=human>siri
+# res2=human<siri
+# many=0
+# if res1:
+# 	many+=1
+# 	print(many,'dram')
+# else:
+# 	many=0
+# 	print(many)
+# 	many=0
+# if res2:
+# 	print(many,'dram')
+# else:
+# 	many=0
+# 	print(many)
+# print(human1,human2,'our number human:',human,'\n',siri1,siri2,'number siri:',siri,'\n','human the win',res1,'\n','siri the win:',res2)
