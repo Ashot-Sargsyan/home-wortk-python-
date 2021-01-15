9368883e4e47dba45ba130a64787ef67ca81edd8
Ashot-Sargsyan <77446410+Ashot-Sargsyan@users.noreply.github.com>
2021-01-15 23:38:05 +0400

    Variables

---
+++ lesspn2homework.py
@@ -0,0 +1,120 @@
+# '''calculating the area of ​​a rectangle'''
+# a = int(input('one side of the rectangle' ))# a one side of the rectangle
+# b = int(input('second side of the rectangle')) #the second side of the rectangle
+# S = a*b
+# result=S
+# print(result)
+
+# '''calculating the area of a square'''
+# a = int(input('parties square')) #parties square
+# S = a**2
+# result = S 
+# print(result)
+
+# '''exchance''' 
+# Dollar=float(input('$is: '))
+# Amd= Dollar*492
+# print(Amd)
+
+# Euro=float(input('€is: '))
+# Amd= Euro*584.63
+# print(Amd)
+
+# Rubl=float(input(' ₽is: '))
+# Amd= Dollar*6.41
+# print(Amd)
+
+'''create 20 legal variables'''
+car='BMW'
+print(car)
+print(type(car))
+
+expelled="AMD"
+print(expelled)
+print(type(expelled))
+
+age='1996'
+print(age)
+print(type(age))
+
+
+age=1998
+print(age)
+print(type(age))
+
+weight="human"
+print(weight)
+print(type(weight))
+
+
+phone='apple_11'
+print(phone)
+print(type(phone))
+
+
+year=301
+print(year)
+print(type(year))
+
+
+construction='magazine'
+print(construction)
+print(type(construction))
+
+fittings=10
+print(fittings)
+print(type(fittings))
+
+
+language=1
+print(language)
+print(type(language))
+
+language='English'
+print(language)
+print(type(language))
+
+# model=1112ea5
+# print(model)
+# print(type(model))
+
+model='1112ea5'
+print(model)
+print(type(model))
+
+
+ship=2020
+print(ship)
+print(type(ship))
+
+
+ship='Titanik'
+print(ship)
+print(type(ship))
+
+
+fighter='F-16'
+print(fighter)
+print(type(fighter))
+
+# fighter=F-16
+# print(fighter)
+# print(type(fighter))
+
+x=True
+print(x)
+print(type(x))
+
+x=5
+y=6
+result=x>y
+print(result)
+print(type(result))
+
+
+x=5
+y=6
+result=x<y
+print(result)
+print(type(result))
+
