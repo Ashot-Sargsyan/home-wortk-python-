b322e1fa01d1b8a19b12b188959ef00236330c94
Ashot-Sargsyan <77446410+Ashot-Sargsyan@users.noreply.github.com>
2021-01-15 23:48:21 +0400

    Logical Operators

---
+++ lesspn6homework.py
@@ -0,0 +1,63 @@
+'''1)We writen a program to check have your name ‘a’ or no'''
+# name='Ash'
+# print('A' in name)
+
+# number='454561288763'
+# print('9' not in number)
+
+
+'''2)We writen a program to check if there  are bananas, oranges and apples in your fridge'''
+# bananas =input('Have you banana in fridge:') == 'yes'
+# print('I have banana',bananas)
+# oranges =input('Have you oranges in fridge:') == 'yes'
+# print('I have oranges',oranges)
+# apples =input('Have you apples in fridge:') == 'yes'
+# print('I have apples',apples)
+
+
+'''3)We write to a program check the following conditions'''
+# import random
+# x=random.randint(-8,15)
+# result=x<=-8 or x>12
+# print('my x =',x,'\n',result)
+
+# y=random.randint(10,50)
+# result=y>10 or y==50
+# print('my y =',y,'\n',result)
+
+# z=random.randint(-9,11)
+# result=z>-10 or z<10
+# print('my z =',z,'\n',result)
+
+# t=random.randint(20,50)
+# result=t!=20 or t>50
+# print('my t =',t,'\n',result)
+
+'''4)We write a program is there any fruit in the refrigerator if not then you need to buy it'''
+# bananas =input('Have you banana in fridge:') == 'yes'
+# print('I have banana',bananas)
+# print('buy bananas in magazine', not bananas)
+# oranges =input('Have you oranges in fridge:') == 'yes'
+# print('I have oranges',oranges)
+# print('buy oranges in magazine',oranges)
+# apples =input('Have you apples in fridge:') == 'yes'
+# print('I have apples',apples)
+# print('buy apples in magazine',not apples)
+# light=input('Have you light at home?' ) == 'yes'
+# print(light)
+# res_bananas=bananas and not light
+# print('Get banana in fridge:',res_bananas)
+# res_oranges=oranges and light
+# print('Get oranges in fridge:',res_oranges)
+# res_apples=apples and not light
+# print('Get apples in fridge:',res_apples)
+
+'''5)'''
+# language=input('What language do you speak?')
+# language=language.upper()
+# print('I speak language a',language)
+'''6)'''
+# language=input('What language do you speak?')
+# language=language.capitalize()
+# print('I speak language a',language)
+
