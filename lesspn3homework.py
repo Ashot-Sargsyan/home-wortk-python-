7d47216fb271deb2bbee52c288111203b80ff775
Ashot-Sargsyan <77446410+Ashot-Sargsyan@users.noreply.github.com>
2021-01-15 23:40:39 +0400

    Mathematical Operators

---
+++ lesspn3homework.py
@@ -0,0 +1,182 @@
+'''№1'''
+# expression='py'
+# name='thon'
+# result=expression*4+name
+# print(result)
+
+'''№2'''
+# example='7p'
+# namber='79'
+# result=example*3+namber
+# print(result)
+
+'''№3'''
+# number='9'
+# print(number*7)
+
+
+'''№4'''
+# MasterCard=0
+# Entrance1=int(input('AMD'))
+# MasterCard+=Entrance1
+# Entrance2=int(input('AMD'))
+# MasterCard+=Entrance2
+# Entrance3=int(input('AMD'))
+# MasterCard+=Entrance3
+# result=MasterCard
+# print(result)
+
+'''№5'''
+# BMW='E52'
+# print(BMW)
+# print(type(BMW))
+
+# number=3.1622257789
+# print(number)
+# print(type(number))
+
+# money=9999999
+# print(money)
+# print(type(money))
+
+# electronic='samsung'
+# print(electronic)
+# print(type(electronic))
+
+# page=336
+# print(page)
+# print(type(page))
+
+# electronic=112234
+# electronic*=4
+# print(electronic)
+# print(type(electronic))
+
+# rock='brilliant'
+# print(rock)
+# print(type(rock))
+
+'''№6'''
+# integer=4
+# print(integer)
+# print(type(integer))
+# fl=5.5
+# print(fl)
+# print(type(fl))
+# result1=integer>fl
+# result2=integer<fl
+# result3=integer==fl
+# print(result1,result2,result3)
+
+# integer=10
+# print(integer)
+# print(type(integer))
+# fl=1.2
+# print(fl)
+# print(type(fl))
+# result1=integer>fl
+# result2=integer<fl
+# result3=integer==fl
+# print(result1,result2,result3)
+
+# integer=8
+# print(integer)
+# print(type(integer))
+# fl=5.5
+# print(fl)
+# print(type(fl))
+# result1=integer>fl
+# result2=integer<fl
+# result3=integer==fl
+# print(result1,result2,result3)
+
+
+# integer=0
+# print(integer)
+# print(type(integer))
+# fl=5.5
+# print(fl)
+# print(type(fl))
+# result1=integer>fl
+# result2=integer<fl
+# result3=integer==fl
+# print(result1,result2,result3)
+
+# integer=0
+# print(integer)
+# print(type(integer))
+# fl=0
+# print(fl)
+# print(type(fl))
+# result1=integer>fl
+# result2=integer<fl
+# result3=integer==fl
+# print(result1,result2,result3)
+
+# integer=25
+# print(integer)
+# print(type(integer))
+# fl=-5
+# print(fl)
+# print(type(fl))
+# result1=integer>fl
+# result2=integer<fl
+# result3=integer==fl
+# print(result1,result2,result3)
+
+# integer=5
+# print(integer)
+# print(type(integer))
+# fl=-3
+# print(fl)
+# print(type(fl))
+# result1=integer>fl
+# result2=integer<fl
+# result3=integer==fl
+# print(result1,result2,result3)
+
+# integer=6
+# print(integer)
+# print(type(integer))
+# fl=-6
+# print(fl)
+# print(type(fl))
+# result1=integer>fl
+# result2=integer<fl
+# result3=integer==fl
+# print(result1,result2,result3)
+
+# integer=3
+# print(integer)
+# print(type(integer))
+# fl=7
+# print(fl)
+# print(type(fl))
+# result1=integer>fl
+# result2=integer<fl
+# result3=integer==fl
+# print(result1,result2,result3)
+
+# integer=0
+# print(integer)
+# print(type(integer))
+# fl=16
+# print(fl)
+# print(type(fl))
+# result1=integer>fl
+# result2=integer<fl
+# result3=integer==fl
+# print(result1,result2,result3)
+
+
+'''№7'''
+# funt=int(input('is a funt :' ))
+# result=funt*0.4535923745
+# print(result,'kilogram')
+
+
+'''№8'''
+# miles=int(input('is a miles :' ))
+# result=miles*1.60934
+# print(result,'kilometers')
+
