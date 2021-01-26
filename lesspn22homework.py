67bce3eae6114d53a6f82820c75f9f03b5b327e8
Ashot-Sargsyan <77446410+Ashot-Sargsyan@users.noreply.github.com>
2021-01-26 23:00:57 +0400

    Python exercises

---
+++ lesspn22homework.py
@@ -0,0 +1,76 @@
+'''1)Write a python function which conversion kilometer to miles.'''
+# def my_function(miles):#im funkcyana
+# 	miles=kilometer*0.621#vorpisi milse ktnem miles = kilometr*0.621
+# 	return miles#veradarcra miles
+
+# kilometer=float(input('Enter how many kilometers you need to convert to miles: '))#mutkenq anum kilometre vor verlucenq miles
+# print(my_function(kilometer))#kanchumeq mer function
+
+
+'''2)Write a python function which conversion days to seconds.'''
+# def my_function(time):
+# 	seconds=days*86400
+# 	return seconds
+# days=int(input('Enter how many days you need to convert to seconds: '))
+# print(my_function(days))
+
+
+
+'''3)Write a python function which generate a valid password'''
+# import random 
+
+# def my_function():
+# 	password=''
+# 	letters='qwertyuioasdfgjklxcnzxvhbkp'
+# 	numbers='123459778956115975238230'
+# 	choice='!@#$%^&*())_+"?><-*/|$#@!^()_*&%#!'
+
+# 	letters_count=6
+# 	numbers_count=choice_count=3
+
+# 	for i in range(letters_count):
+# 		l=random.choice(letters)
+# 		password+=l
+
+# 	for i in range(numbers_count):
+# 		n=random.choice(numbers)
+# 		c=random.choice(choice)
+# 		password+=n
+# 		password+=c
+
+# 	password=list(password)
+# 	random.shuffle(password)
+# 	return''.join(password)
+# print(my_function())
+
+
+
+'''4)Factorial'''
+# def my_function(factorial):
+
+# 	res=1
+
+# 	for i in range(2,factorial+1):
+# 		res*=i
+# 	return res
+
+# number=int(input('write your number : '))
+# print(my_function(number))
+
+
+
+'''5)Given an list of numbers.Find the maximum element in list.Without use max function'''
+
+# def my_function(mylist):
+
+# 	res=list1[0]#es toxe incha anum?
+
+# 	for i in list1:
+# 		if i>res:
+# 			res=i
+# 	return res
+
+# list1=[1,5,9,7,5,3,2,5,8,6,5,4]
+# print(my_function(list1))
+
+
