a062e2b0de1de45576d711b6ec9ee3b911be6661
Ashot-Sargsyan <77446410+Ashot-Sargsyan@users.noreply.github.com>
2021-02-03 01:14:09 +0400

    Modules Python

---
+++ lesspn28homework.py
@@ -0,0 +1,29 @@
+'''1)Create a python function factorial and import this file in another file and print factorial.'''
+# import modul
+# print(modul.factorial(5))
+
+
+'''2)Write a Python function to calculate surface volume and area of a cylinder.V=πr^2h and A=2πrh+2πr^2 '''
+# import modul
+# height=float(input('write height: '))
+# radian=float(input('write radian: '))
+# print(modul.cylinder(height,radian))
+
+
+'''3)Write a Python function to calculate surface volume and area of a sphere. V = 4/3*π*r**3 and A = 4*π*r**2'''
+# import modul
+# radius=float(input('write your radius: '))
+# print(modul.surface(radius))
+
+
+'''4)Write a Python function to convert degree to radian. one radian = pi/180: 90 radian = 1.57...'''
+# import modul
+# degree=float(input('Write the degree you want to convert radian: '))
+# print(modul.convert(degree))
+
+
+'''5)Write a Python function to print all primes smaller than or equal to a specified number. Call function:numbers(9) Output: (2, 3, 5, 7)'''
+import modul
+number1=int(input('number1:'))
+number2=int(input('number2:'))
+print(modul.primes(number1,number2))
